5f285d84cd10e0549943efefb7a0ba1d3330a0f6
Jayar <jrsaranza92@gmail.com>
2024-05-21 19:29:54 +0800

    try 4

--- mealprep.php
+++ mealprep.php
@@ -107,7 +107,7 @@ $(document).ready(function(){
     // Function to fetch user data and calculate suggested intakes
     function fetchUserDataAndCalculateIntakes() {
         $.ajax({
-            url: "php/fetch_user_data.php", // Adjust the path to your PHP file
+            url: "php/fetch_user_id.php", // Adjust the path to your PHP file
             type: "GET",
             success: function(response) {
                 const userData = JSON.parse(response);
